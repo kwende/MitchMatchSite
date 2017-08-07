85fcdcf0a39eb65f1aa4b98cdd8e2e69697951a5
Ben <ben@ben-rush.net>
2017-08-07 16:37:56 -0500

    Blah

--- Site/app/views.py
+++ Site/app/views.py
@@ -7,14 +7,19 @@ from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
 
+from app.models import Record
+
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
+
+    item = Record.objects.filter(id = 112275)[0]
+
     return render(
         request,
         'app/index.html',
         {
-            'title':'Home Page',
+            'title':item.FirstName,
             'year':datetime.now().year,
         }
     )
